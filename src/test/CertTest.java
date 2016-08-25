7d69add447878efba3f477ce1f1a81cc5651b8f3
shepherdviolet <ryan.n@163.com>
2016-08-25 14:45:46 +0800

    测试修改

--- src/test/CertTest.java
+++ src/test/CertTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.asn1.x509.X509CertificateStructure;
 import sviolet.smcrypto.exception.InvalidCertificateException;
 import sviolet.smcrypto.exception.InvalidKeyDataException;
 import sviolet.smcrypto.exception.InvalidSignDataException;
 import sviolet.smcrypto.sm2.SM2Cipher;
 import sviolet.smcrypto.util.Base64Utils;
+import sviolet.smcrypto.util.CertificateUtils;
 
 import java.io.IOException;
 
@@ -13,20 +16,59 @@ import java.io.IOException;
  */
 public class CertTest {
 
-    private static final String certData = "MIICQDCCAeWgAwIBAgIQG2THdO0arf/KaLKoTVlCOzAMBggqgRzPVQGDdQUAMB8xEDAOBgNVBAMMB1NNMlJPT1QxCzAJBgNVBAYTAkNOMB4XDTE0MDYxODEzNTgzNVoXDTE2MDYxODEzNTgzNVowZTEiMCAGCSqGSIb3DQEJARYTam9ubGxlbkBob3RtYWlsLmNvbTEPMA0GA1UEBwwG6ZW/5rKZMQ8wDQYDVQQIDAbmuZbljZcxCzAJBgNVBAYTAkNOMRAwDgYDVQQDDAdKb25sbGVuMFkwEwYHKoZIzj0CAQYIKoEcz1UBgi0DQgAEXLAuT39XB5LJmTprFiGLPfqZl5tyGm1n9oXSVDrGRP2RfQRJOqD6cH6PEvmGhM1ydJv0iQMg2mvh6PjAlm58W6OBujCBtzAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBQstXX3UIwlBK9k70GJYGM8mjG6gTAfBgNVHSMEGDAWgBQ/LpOmNnAJt7mAwIZpsX3cnqlkqjBCBggrBgEFBQcBAQQ2MDQwMgYIKwYBBQUHMAKGJmh0dHA6Ly9sb2NhbGhvc3QvUEtJL2NlcnRzL0RTQVJPT1QuY3J0MA4GA1UdDwEB/wQEAwIE8DATBgNVHSUEDDAKBggrBgEFBQcDAjAMBggqgRzPVQGDdQUAA0cAMEQCIBJO7K/XDt+igzKkWSkbRKZRtQKsS1i2Fmdp2Ar5EEL+AiA759mE/uINaEC7sMXOoqzTzkLCMIHGyLi80j0dG5pjow==";
-
     public static void main(String[] args) throws IOException, InvalidCertificateException, InvalidKeyDataException, InvalidSignDataException {
+        certVerify();
+        System.out.println("\n\n");
+        dataVerify();
+    }
 
-        String data = "jonllen";
-        String sign = "64LTP1CphtWrfHnFB3OMyEuV1+ei5DpBPZY39VeQUb9V6yRYP3SbpAWivrpP1q5j0D8b4xQid4327TK9NvEbrA==";
+    //证书验证的证书
+    private static final String certData = "MIICaDCCAgygAwIBAgIJAK8ocl2Y0zFDMAwGCCqBHM9VAYN1BQAwfTELMAkGA1UEBgwCY24xCzAJBgNVBAgMAmJqMQswCQYDVQQHDAJiajEPMA0GA1UECgwGdG9wc2VjMQ8wDQYDVQQLDAZ0b3BzZWMxETAPBgNVBAMMCFRvcHNlY0NBMR8wHQYJKoZIhvcNAQkBDBBiakB0b3BzZWMuY29tLmNuMB4XDTEyMDYyNDA3NTQzOVoXDTMyMDYyMDA3NTQzOVowfTELMAkGA1UEBgwCY24xCzAJBgNVBAgMAmJqMQswCQYDVQQHDAJiajEPMA0GA1UECgwGdG9wc2VjMQ8wDQYDVQQLDAZ0b3BzZWMxETAPBgNVBAMMCFRvcHNlY0NBMR8wHQYJKoZIhvcNAQkBDBBiakB0b3BzZWMuY29tLmNuMFkwEwYHKoZIzj0CAQYIKoEcz1UBgi0DQgAE1pwvHuw7+2uVswwoCFx3sSXXepw5Ul2BkHaPN9ayBbWJ3NMWu+fYmp3CGRfxd5nmmFMfXm4+EL0xNwslnD+Bw6NzMHEwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUjl2QNHhYuqrYcNi9+6aoXntWO2QwHwYDVR0jBBgwFoAUjl2QNHhYuqrYcNi9+6aoXntWO2QwCwYDVR0PBAQDAgEGMBEGCWCGSAGG+EIBAQQEAwIAVzAMBggqgRzPVQGDdQUAA0gAMEUCIQCGqTACsUVb7KXmi8E5pqJtyWpKj1Mm0vPokvH6ondQKwIgFH5vrHQjncD1e9avYQBSF9fxplgZ/tdU9nxDgh2HnRU=";
+
+    private static void certVerify() throws IOException, InvalidSignDataException, InvalidKeyDataException {
         byte[] certBytes = Base64Utils.decode(certData);
+
+        System.out.println("证书数据:" + ByteUtils.bytesToHex(certBytes));
+        //自签名根证书
+        X509CertificateStructure certInfo = CertificateUtils.parseX509(certBytes);
+        System.out.println("证书版本:" + certInfo.getVersion());
+        System.out.println("序列号:" + certInfo.getSerialNumber().getValue().toString(16));
+        System.out.println("算法标识:" + certInfo.getSignatureAlgorithm().getObjectId().getId());
+        System.out.println("签发者:" + certInfo.getIssuer());
+        System.out.println("开始时间:" + certInfo.getStartDate().getTime());
+        System.out.println("结束时间:" + certInfo.getEndDate().getTime());
+        System.out.println("主体名:" + certInfo.getSubject());
+        System.out.println("签名值:" + ByteUtils.bytesToHex(certInfo.getSignature().getBytes()));
+
+        SubjectPublicKeyInfo publicInfo = certInfo.getSubjectPublicKeyInfo();
+        System.out.println("主体标识符:" + publicInfo.getAlgorithmId().getObjectId().getId());
+        System.out.println("主体公钥值:" + ByteUtils.bytesToHex(publicInfo.getPublicKeyData().getBytes()));
+
+        //验签无效, 因为国密证书验签是还需要填充数据, 并不是简单的将前面内容进行验签
+//        byte[] certContent = new byte[certBytes.length - 71];//证书内容
+//        byte[] certSign = certInfo.getSignature().getBytes();//证书的签名71bytes
+//        byte[] parentPublicKey = publicInfo.getPublicKeyData().getBytes();//由于是根证书, 因此主体公钥即为根证书公钥, 如果非根证书, 则需要父级证书的公钥
+//
+//        System.arraycopy(certBytes, 0, certContent, 0, certContent.length);
+//
+//        SM2Cipher cipher = new SM2Cipher(SM2Cipher.Type.C1C3C2);
+//        System.out.println(cipher.verifySignByASN1(null, parentPublicKey, certContent, certSign));//证书内的签名字段为ASN.1编码
+    }
+
+    //数据验签的证书
+    private static final String certData2 = "MIICQDCCAeWgAwIBAgIQG2THdO0arf/KaLKoTVlCOzAMBggqgRzPVQGDdQUAMB8xEDAOBgNVBAMMB1NNMlJPT1QxCzAJBgNVBAYTAkNOMB4XDTE0MDYxODEzNTgzNVoXDTE2MDYxODEzNTgzNVowZTEiMCAGCSqGSIb3DQEJARYTam9ubGxlbkBob3RtYWlsLmNvbTEPMA0GA1UEBwwG6ZW/5rKZMQ8wDQYDVQQIDAbmuZbljZcxCzAJBgNVBAYTAkNOMRAwDgYDVQQDDAdKb25sbGVuMFkwEwYHKoZIzj0CAQYIKoEcz1UBgi0DQgAEXLAuT39XB5LJmTprFiGLPfqZl5tyGm1n9oXSVDrGRP2RfQRJOqD6cH6PEvmGhM1ydJv0iQMg2mvh6PjAlm58W6OBujCBtzAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBQstXX3UIwlBK9k70GJYGM8mjG6gTAfBgNVHSMEGDAWgBQ/LpOmNnAJt7mAwIZpsX3cnqlkqjBCBggrBgEFBQcBAQQ2MDQwMgYIKwYBBQUHMAKGJmh0dHA6Ly9sb2NhbGhvc3QvUEtJL2NlcnRzL0RTQVJPT1QuY3J0MA4GA1UdDwEB/wQEAwIE8DATBgNVHSUEDDAKBggrBgEFBQcDAjAMBggqgRzPVQGDdQUAA0cAMEQCIBJO7K/XDt+igzKkWSkbRKZRtQKsS1i2Fmdp2Ar5EEL+AiA759mE/uINaEC7sMXOoqzTzkLCMIHGyLi80j0dG5pjow==";
+
+    private static void dataVerify() throws InvalidCertificateException, InvalidSignDataException, InvalidKeyDataException {
+        //数据验签
+        String data = "jonllen";
+        String sign = "BOuC0z9QqYbVq3x5xQdzjMhLldfnouQ6QT2WN/VXkFG/VeskWD90m6QFor66T9auY9A/G+MUIneN9u0yvTbxG6w=";//0x04 + r + s 格式的签名
+        byte[] certBytes = Base64Utils.decode(certData2);
         byte[] signBytes = Base64Utils.decode(sign);
-        System.out.println(ByteUtils.bytesToHex(certBytes));
-        System.out.println(ByteUtils.bytesToHex(signBytes));
+        System.out.println("certBytes:" + ByteUtils.bytesToHex(certBytes));
+        System.out.println("signBytes:" + ByteUtils.bytesToHex(signBytes));
 
         SM2Cipher cipher = new SM2Cipher(SM2Cipher.Type.C1C3C2);
         System.out.println(cipher.verifySignByX509Cert(certBytes, data.getBytes(), signBytes));
-
     }
 
 }
